9435c2239513230afe84e8b12f2abfcd4fbf29f3
Younhwan(PC) <gihan56@ajou.ac.kr>
2024-05-28 19:08:07 +0900

    :sparkles: Notice Upload/Modify UI 및 기능 구현

--- app/src/main/java/com/ajou/helptmanager/notice/NoticeViewModel.kt
+++ app/src/main/java/com/ajou/helptmanager/notice/NoticeViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ajou.helptmanager.UserDataStore
 import com.ajou.helptmanager.network.RetrofitInstance
 import com.ajou.helptmanager.network.api.NoticeService
 import kotlinx.coroutines.async
