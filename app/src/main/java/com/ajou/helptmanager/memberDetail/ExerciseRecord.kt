94dd823e8f7689ceb112eaa967686e65c68c962b
YoungHwna (PC) <gihan56@ajou.ac.kr>
2024-06-03 17:01:12 +0900

    :sparkles: ExerciseRecord.kt 수정 1. recordDate 타입 LocalDate -> String 2. successRate 타입 Int -> Float

--- app/src/main/java/com/ajou/helptmanager/memberDetail/ExerciseRecord.kt
+++ app/src/main/java/com/ajou/helptmanager/memberDetail/ExerciseRecord.kt
@@ -1,15 +1,13 @@
 package com.ajou.helptmanager.memberDetail
 
-import java.time.LocalDate
-
 data class ExerciseRecord(
     val equipmentName: String,
     val count: Int,
     val setNumber: Int,
     val weight: Int,
     val recordTime: String,
-    val recordDate: LocalDate,
-    val successRate: Int,
+    val recordDate: String,
+    val successRate: Float,
     val comment: String,
     val snapshotFile: String
 )
